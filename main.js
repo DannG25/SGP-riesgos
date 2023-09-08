875a71a68fac56520be61559fde4eb946f9d0e6c
Deyner Romero <deyner.romero@sinco.com.co>
2023-09-08 16:57:39 -0500

    se pinta la grafica

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
-
-
-const ctx = document.getElementById(myChart);
-
+const ctx = document.getElementById('myChart');
 const data = {
     labels: [
-      'Muy alto',
-      'Medio alto',
-      'Medio',
-      'Medio Bajo',
-      'Medio Bajo',
-      'Muy bajo',
-      'Sin identificar',
+        'Muy alto',
+        'Medio alto',
+        'Medio',
+        'Medio Bajo',
+        'Medio Bajo',
+        'Muy bajo',
+        'Sin identificar',
     ],
     datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgba(229, 1, 39, 0.5)',
-        'rgba(255, 154, 31, 0.5)',
-        'rgba(255, 251, 90, 0.5)',
-        'rgba(47, 231, 65, 0.26)',
-        'rgba(47, 231, 65, 0.7)',
-        'rgba(144, 130, 122, 0.5)',
-      ],
-      hoverOffset: 4
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgba(229, 1, 39, 0.5)',
+            'rgba(255, 154, 31, 0.5)',
+            'rgba(255, 251, 90, 0.5)',
+            'rgba(47, 231, 65, 0.26)',
+            'rgba(47, 231, 65, 0.7)',
+            'rgba(144, 130, 122, 0.5)',
+        ],
+        hoverOffset: 4
     }]
-  };
+};
+
+new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+})
