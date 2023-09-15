9f51316a8320632c0c04a1a56755620929e7e431
DannG25 <125318162+DannG25@users.noreply.github.com>
2023-09-15 16:02:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,29 @@ const data = {
         'Medio alto',
         'Medio',
         'Medio Bajo',
-        'Medio Bajo',
-        'Muy bajo',
+        'Muy Bajo',
         'Sin identificar',
     ],
+
     datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
+        labels: [
+            'Muy alto',
+            'Medio alto',
+            'Medio',
+            'Medio Bajo',
+            'Muy Bajo',
+            'Sin identificar',
+        ],
+        data: [30, 15, 21, 13, 19, 45],
+        borderColor: [
+        'rgba(229, 1, 39, 0.5)',
+        'rgba(255, 154, 31, 0.5)',
+        'rgba(255, 251, 90, 0.5)',
+        'rgba(47, 231, 65, 0.26)',
+        'rgba(47, 231, 65, 0.7)',
+        'rgba(144, 130, 122, 0.5)',
+        ],
+             
         backgroundColor: [
             'rgba(229, 1, 39, 0.5)',
             'rgba(255, 154, 31, 0.5)',
@@ -20,7 +36,7 @@ const data = {
             'rgba(47, 231, 65, 0.7)',
             'rgba(144, 130, 122, 0.5)',
         ],
-        hoverOffset: 4
+        hoverOffset: 5
     }]
 };
 
