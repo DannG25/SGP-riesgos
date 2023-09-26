2e817dcc71b30ab1c5797791f9d4642bb3b79472
DannG25 <125318162+DannG25@users.noreply.github.com>
2023-09-26 12:01:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 const ctx = document.getElementById('myChart');
+ctx.style.width = '100%'
+ctx.style.height = 'auto'
 const data = {
-    labels: [
-        'Muy alto',
-        'Medio alto',
-        'Medio',
-        'Medio Bajo',
-        'Muy Bajo',
-        'Sin identificar',
-    ],
-
+    labels: ['Muy alto', 'Medio alto', 'Medio', 'Medio Bajo', 'Muy Bajo', 'Sin identificar',],
     datasets: [{
         labels: [
             'Muy alto',
@@ -18,17 +12,18 @@ const data = {
             'Muy Bajo',
             'Sin identificar',
         ],
-        label:'Riesgos',
+        label: 'Riesgos',
         data: [30, 15, 21, 13, 19, 45],
         borderColor: [
-        'rgba(229, 1, 39, 0.5)',
-        'rgba(255, 154, 31, 0.5)',
-        'rgba(255, 251, 90, 0.5)',
-        'rgba(47, 231, 65, 0.26)',
-        'rgba(47, 231, 65, 0.7)',
-        'rgba(144, 130, 122, 0.5)',
+            'rgba(229, 1, 39, 0.5)',
+            'rgba(255, 154, 31, 0.5)',
+            'rgba(255, 251, 90, 0.5)',
+            'rgba(47, 231, 65, 0.26)',
+            'rgba(47, 231, 65, 0.7)',
+            'rgba(144, 130, 122, 0.5)',
         ],
-             
+        // cutout: [35],    
+        borderAlign: ['inner'],
         backgroundColor: [
             'rgba(229, 1, 39, 0.5)',
             'rgba(255, 154, 31, 0.5)',
@@ -37,10 +32,21 @@ const data = {
             'rgba(47, 231, 65, 0.7)',
             'rgba(144, 130, 122, 0.5)',
         ],
-      hoverOffset: 15
+        hoverOffset: 10
+
     }]
-}; 
-    const chart = new Chart(ctx, {
+};
+
+const chart = new Chart(ctx, {
     type: 'doughnut',
     data: data,
+    options: {
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'left',
+            }
+        }
+    }
 });
