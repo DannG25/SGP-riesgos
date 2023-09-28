39f6a9c03d53eea7c61b7328791e99ebac803ea2
DannG25 <125318162+DannG25@users.noreply.github.com>
2023-09-28 11:11:42 -0500

    Update matriz-riesgos.js

--- matriz-riesgos.js
+++ matriz-riesgos.js
@@ -1,15 +1,19 @@
-
-const prob = [ 0.10, 0.30,0.50,0.70,0.90 ]
-const impact = [ 0.90, 0.70, 0.50, 0.30, 0.10 ] 
+const prob = [ 0.10, 0.30, 0.50, 0.70, 0.90 ];
+const impact = [ 0.05, 0.10, 0.20, 0.40, 0.80 ];
 
 const table = document.createElement('table');
-impact.forEach(item => {
-    const impact = document.createElement('tr');
-    prob.forEach(col => {
+prob.forEach(item => {
+    const prob = document.createElement('tr');
+    impact.forEach(col => {
         const celda = document.createElement('td');
         celda.innerHTML = item * col;
-        impact.append(celda);
+        prob.append(celda);
     })
-    table.append(impact)
+    table.append(prob);
 });
+
 document.body.appendChild(table);
+
+
+// const probabilidades = [ 0.10, 0.30, 0.50, 0.70, 0.90 ];
+// const impacto = [ 0.05, 0.10, 0.20, 0.40, 0.80 ];  
